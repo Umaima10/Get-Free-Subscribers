04672590480fc102fb322dbe382824249e799eb2
Umaima10 <125506621+Umaima10@users.noreply.github.com>
2023-02-20 15:41:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ Let btn = document.getElementByid("btn");
 btn.addEventListener("click", myFunction);
 
 function myFunction() {
-  document.getElementById("").innerHTML = "";
+document.getElementById("input1").innerHTML = "";
+document.getElementById("input2").innerHTML = "";
+document.getElementById("input3").innerHTML = "";
+document.getElementById("input4").innerHTML = "";
 }
 
  function resetForm() {
